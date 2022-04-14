40e0c6b19609e44057c9d5db147d6e93748fbd08
PhongNguyenTrung <90634242+PhongNguyenTrung@users.noreply.github.com>
2022-04-14 08:32:37 +0700

    a

--- components/Fan.js
+++ components/Fan.js
@@ -1,30 +1,128 @@
 import React, { useState } from "react";
-import { View, Switch, StyleSheet, Text, SafeAreaView, Image } from "react-native";
+import { View, Switch, StyleSheet, Text, SafeAreaView, Image, TouchableOpacity } from "react-native";
 import Ionicons from 'react-native-vector-icons/MaterialCommunityIcons';
 import RadioGroup from 'react-native-radio-buttons-group';
+import axios from 'axios';
 
-const radioButtonsData = [{
-    id: '1', // acts as primary key, should be unique and non-empty string
-    label: 'Low',
-    value: 'option1'
-}, {
-    id: '2',
-    label: 'Medium',
-    value: 'medium'
-}, {
-    id: '3',
-    label: 'High',
-    value: 'high'
-}]
+function RadioButton({selected, optionName}) {
+    return (
+        <View style={{
+            flexDirection: 'row',
+        }}>
+            <View style={[{
+                height: 24,
+                width: 24,
+                borderRadius: 12,
+                borderWidth: 2,
+                borderColor: '#000',
+                alignItems: 'center',
+                marginRight: 8,
+                justifyContent: 'center',
+            }]}>
+                {
+                selected ?
+                    <View style={{
+                    height: 12,
+                    width: 12,
+                    borderRadius: 6,
+                    backgroundColor: '#000',
+                    }}/>
+                    : null
+                }
+            </View> 
+            <View style={{ marginRight: 24 }}>
+                <Text style={{ fontSize: 16 }}>
+                    {optionName}
+                </Text>
+            </View>
+        </View>
+    );
+}
+const FanButton = ( {state} ) => {
+    const [isEnabled, setIsEnabled] = useState(state != 7);
+    const toggleSwitch = () => {
+        let x;
+        setIsEnabled(previousState => !previousState);
+        axios.get('https://io.adafruit.com/api/v2/phongnguyen2001/feeds/fan/data')
+        .then(data=>{
+            x = data.data[0].value;
+            if(x == 7) x = 8;
+            else x = 7;
+            axios({
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-AIO-Key': 'aio_Gsvl46SPpX844VDMJFUb2PbvzeZR'
+                },
+                url: 'https://io.adafruit.com/api/v2/phongnguyen2001/feeds/fan/data',
+                data: JSON.stringify({ "value": x })
+            }).then(data=>{console.log("success")}
+            )
+        })
+        if (isEnabled) {
+            setLow(false);
+            setMedium(false);
+            setHigh(false);
+        }
+        else setLow(true);
+    }
 
-const FanButton = () => {
-    const [isEnabled, setIsEnabled] = useState(false);
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-   
-    const [radioButtons, setRadioButtons] = useState(radioButtonsData)
+    const [low, setLow] = useState(state == 8);
+    const [medium, setMedium] = useState(state == 9);
+    const [high, setHigh] = useState(state == 10);
 
-    function onPressRadioButton(radioButtonsArray) {
-        setRadioButtons(radioButtonsArray);
+    const onPressLow = () => {
+        if (isEnabled) {
+            setLow(true);
+            setMedium(false);
+            setHigh(false);
+            axios({
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-AIO-Key': 'aio_Gsvl46SPpX844VDMJFUb2PbvzeZR'
+                },
+                url: 'https://io.adafruit.com/api/v2/phongnguyen2001/feeds/fan/data',
+                data: JSON.stringify({ "value": 8 })
+            }).then(data=>{console.log("success")}
+            )
+        }
+    }
+
+    const onPressMedium = () => {
+        if (isEnabled) {
+            setLow(false);
+            setMedium(true);
+            setHigh(false);
+            axios({
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-AIO-Key': 'aio_Gsvl46SPpX844VDMJFUb2PbvzeZR'
+                },
+                url: 'https://io.adafruit.com/api/v2/phongnguyen2001/feeds/fan/data',
+                data: JSON.stringify({ "value": 9 })
+            }).then(data=>{console.log("success")}
+            )
+        }
+    }
+
+    const onPressHigh = () => {
+        if (isEnabled) {
+            setLow(false);
+            setMedium(false);
+            setHigh(true);
+            axios({
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-AIO-Key': 'aio_Gsvl46SPpX844VDMJFUb2PbvzeZR'
+                },
+                url: 'https://io.adafruit.com/api/v2/phongnguyen2001/feeds/fan/data',
+                data: JSON.stringify({ "value": 10 })
+            }).then(data=>{console.log("success")}
+            )
+        }
     }
    
     
@@ -75,27 +173,25 @@ const FanButton = () => {
                             </View>
 
                         </View>
-                        
-                        <RadioGroup 
-                            layout='row'
-                            radioButtons={radioButtons} 
-                            onPress={onPressRadioButton} 
-                        />
-                        
-                        <Text style={{
-                            fontSize: 16,
-                            marginTop: 16,
-                            marginLeft: 10,
-                            }}>
-                                Turned on 3 hours ago
-                        </Text>
+                
+
+                        <View style={{ flexDirection: 'row' }}>
+                            
+                            <TouchableOpacity onPress={onPressLow}>
+                                <RadioButton selected={low} optionName={"Low"}/>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity onPress={onPressMedium}>
+                                <RadioButton selected={medium} optionName={"Medium"}/>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity onPress={onPressHigh}>
+                                <RadioButton selected={high} optionName={"High"}/>
+                            </TouchableOpacity>
+                        </View>
+                    
                     </View>
                 </SafeAreaView>
-
-                {/* <Image 
-                    source={require('../assets/img/profile-pic.png')}
-                    style={styles.profileImage}
-                /> */}
 
             </View>
         </SafeAreaView>
@@ -130,71 +226,3 @@ const styles = StyleSheet.create({
 });
 
 export default FanButton;
-
-
-
-// import * as React from 'react';
-// import { View, Text, StyleSheet, SafeAreaView, Image, Switch } from 'react-native';
-// import Ionicons from 'react-native-vector-icons/Ionicons';
-
-// export default function SwitchButton() {
-//     const [isEnabled, setIsEnabled] = useState(false);
-//     const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-
-//     return (
-//         <SafeAreaView>
-//             <View style={[styles.headerWrapper, {paddingTop: 10}]}>
-//                 <SafeAreaView>
-//                     <View style={styles.switchContainer}>
-//                         <View style={styles.switchHeader}>
-//                             {/* icon */}
-//                             <View>
-//                                 <Ionicons name='home' style={styles.sensorData}/>
-//                             </View>
-
-//                             {/* switch name */}
-//                             <Text>Door</Text>
-
-//                             {/* Switch */}
-//                             <View style={styles.container}>
-//                                 <Switch
-//                                     trackColor={{ false: "#767577", true: "#81b0ff" }}
-//                                     thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-//                                     ios_backgroundColor="#3e3e3e"
-//                                     onValueChange={toggleSwitch}
-//                                     value={isEnabled}
-//                                 />
-//                             </View>
-//                         </View>
-//                         <Text>Opened 3 hours ago</Text>
-//                     </View>
-//                 </SafeAreaView>
-
-//                 {/* <Image 
-//                     source={require('../assets/img/profile-pic.png')}
-//                     style={styles.profileImage}
-//                 /> */}
-
-//             </View>
-//         </SafeAreaView>
-//     );
-// }
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         alignItems: "center",
-//         justifyContent: "center"
-//     },
-
-//     headerWrapper: {
-//         paddingHorizontal: 24,
-//     },
-
-//     title: {
-//         fontSize: 20, 
-//         fontWeight: 'bold'
-//     },
-
-
-// });
--- navigation/screens/Home.js
+++ navigation/screens/Home.js
@@ -6,6 +6,44 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import Header from '../../components/Header'
 import SwitchButton from '../../components/Switch'
 import FanButton from '../../components/Fan'
+import axios from 'axios';
+
+let ledroom, ledhallway, door, light, humi, temp, fanstate;
+
+async function lastValueLedroom(){
+    const data = await axios.get('https://io.adafruit.com/api/v2/phongnguyen2001/feeds/ledroom/data');
+    if(data.data[0].value == 1) ledroom = true;
+    else ledroom = false;
+    console.log()
+}
+async function lastValueLedHallway(){
+    const data = await axios.get('https://io.adafruit.com/api/v2/phongnguyen2001/feeds/ledoutdoor/data');
+    if(data.data[0].value == 3) ledhallway = true;
+    else ledhallway = false;
+}
+async function lastValueDoor(){
+    const data = await axios.get('https://io.adafruit.com/api/v2/phongnguyen2001/feeds/door/data');
+    if(data.data[0].value == 5) door = true;
+}
+async function lightsensor(){
+    const data = await axios.get('https://io.adafruit.com/api/v2/phongnguyen2001/feeds/light-sensor/data');
+    light = data.data[0].value;
+}
+async function temperaturesensor(){
+    const data = await axios.get('https://io.adafruit.com/api/v2/phongnguyen2001/feeds/temp-sensor/data');
+    temp = data.data[0].value;
+    console.log(temp);
+}
+async function lastfanvalue(){
+    const data = await axios.get('https://io.adafruit.com/api/v2/phongnguyen2001/feeds/fan/data');
+    fanstate = data.data[0].value; 
+}
+lastfanvalue();
+lastValueDoor();
+lastValueLedHallway();
+lastValueLedroom();
+lightsensor();
+temperaturesensor();
 
 export default function Home() {
     return (
@@ -23,7 +61,7 @@ export default function Home() {
                         <View style={[styles.sensor, {borderTopLeftRadius: 20}]}> 
                             <SafeAreaView style={{alignItems: 'center', justifyContent: 'center', paddingTop: 10}}>
                                 <Text style={styles.sensorData}>
-                                    <Ionicons name='thunderstorm' style={styles.sensorData}/> 30 °C
+                                    <Ionicons name='thunderstorm' style={styles.sensorData}/> {temp} °C
                                 </Text>
                                 <Text style={{color: 'white'}}>
                                     Temperature
@@ -34,7 +72,7 @@ export default function Home() {
                         <View style={[styles.sensor, {borderTopRightRadius: 20}]}> 
                             <SafeAreaView style={{alignItems: 'center', justifyContent: 'center', paddingTop: 10}}>
                                 <Text style={styles.sensorData}>
-                                    <Ionicons name='sunny' style={styles.sensorData}/> 5 cd
+                                    <Ionicons name='sunny' style={styles.sensorData}/> {light} cd
                                 </Text>
                                 <Text style={{color: 'white'}}>
                                     Light intensity
@@ -75,8 +113,8 @@ export default function Home() {
                         <Text style={[styles.title, {paddingHorizontal: 24, paddingTop: 24}]}>
                             Devices on
                         </Text>
-                        <SwitchButton switchName={'Door'} time={'3 hours'}  state={true}/>           
-                        <SwitchButton switchName={'ClassroomLight'} time={'3 hours'} state={true}/>            
+                        <SwitchButton switchName={'Door'} time={'3 hours'}  state={door} />
+                        <SwitchButton switchName={'ClassroomLight'} time={'3 hours'} state={ledroom}/>            
                         {/* <FanButton />            */}
                         
                     </View>
